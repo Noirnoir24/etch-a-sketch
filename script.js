76f7e59aeb43de29e34c47c7b7640c6cae535a84
Noirnoir24 <184695851+Noirnoir24@users.noreply.github.com>
2025-05-08 10:16:10 -0600

    Add button to html,add css rules,modify js file
    Add button that allows user to choose dimensions
    for canvas and replace previous canvas for new one
    Add two css rules for styling.

--- script.js
+++ script.js
@@ -6,10 +6,52 @@ for(i=0; i<256; i++) {
     canvas.appendChild(grid)
 }
 
-const everySquare= document.querySelectorAll(".square")
+let oldSquares= document.querySelectorAll(".square")
 
-for (let square of everySquare) {     
+for (let square of oldSquares) {     
     square.addEventListener("mouseover", () => {         
         square.style.backgroundColor = "red";      
     }); 
 }
+
+
+
+const button= document.querySelector("#changeSize")
+
+button.addEventListener("click", () => {
+    let dimension= prompt("How many squares per side? (0-100)");
+    while(dimension>100 || dimension<=0) {dimension= prompt("How many squares per side? (1-100)");}
+    let amountOfSquares= dimension*dimension
+    
+    for(let square of oldSquares) {
+        canvas.removeChild(square)
+    }
+
+    let size= 400/dimension;
+
+    for(i=0; i<amountOfSquares; i++) {
+        const grid= document.createElement("div");
+        grid.setAttribute('style', `
+            width: ${size}px; 
+            height: ${size}px; 
+            box-sizing: border-box;
+            border: 1px solid #ddd;
+            background-color: lightgray;
+        `);
+        grid.classList.add("square")
+        canvas.appendChild(grid)
+    }
+
+    let newSquares= document.querySelectorAll(".square")
+
+    for (let square of newSquares) {     
+        square.addEventListener("mouseover", () => {         
+            square.style.backgroundColor = "red";      
+        }); 
+    }
+
+    oldSquares=newSquares
+
+})
+
+
